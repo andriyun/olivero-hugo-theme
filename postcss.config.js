33e72327c0816d31e8c0c021e23eb12de80c5b29
Andríyun <ayu@danskecommodities.com>
2023-12-20 02:08:42 +0100

    Sorted out postcss and js
    Signed-off-by: Andríyun <ayu@danskecommodities.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-    plugins: [
-      require('autoprefixer')
-    ]
-  };
-  
-  
+  plugins: [
+    require('postcss-import')({
+      path: ['./themes/olivero-hugo-theme/assets/css'], // where to look for the css assets
+    }),
+    require('postcss-nested'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties')({
+      // preserve: false,
+    }),
+    require('postcss-calc'),
+    require('autoprefixer')({
+      grid: 'no-autoplace',
+    }),
+    require('perfectionist')({
+      indentSize: 2,
+    }),
+    // require('postcss-rtl'),
+  ],
+};
